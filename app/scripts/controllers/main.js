88577cd136372105f1cc78fd56659d9d4f3f4bd6
zirbem07 <maxwell.zirbel@gmail.com>
2014-07-16 22:47:50 -0700

    commented main ctrl

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,24 +10,25 @@
 angular.module('realTimeTriviaApp')
   .controller('MainCtrl', function ($scope, $firebase) {
 
-
+        //reference to firebase
         var questionsRef = new Firebase("https://maxwellzirbel.firebaseio.com/questions");
-        $scope.q = ["This is question 1", "this is question 2", "I will make these objects", "eventually"];
 
+        //these items will get added to the firebase
         $scope.makeList = function(ref) {
-
+            $scope.q = ["This is question 1", "this is question 2", "I will make these objects", "eventually"];
             for (var i = 0; i < $scope.q.length; i++) {
                 ref.push($scope.q[i]);
             }
         }
 
+        //this gets the first item from the list for displaying
         $scope.getFirstFromList = function(ref, cb) {
             ref.startAt().limit(1).once("child_added", function(snapshot) {
                 cb(snapshot.val());
             });
         }
 
-        // Running this should popup an alert with "banana".
+        //this calls make list and get first item and then alerts the value
         $scope.go = function() {
             $scope.makeList(questionsRef);
             $scope.getFirstFromList(questionsRef, function (val) {
@@ -36,17 +37,9 @@ angular.module('realTimeTriviaApp')
         }
 
         $scope.questions = $firebase(questionsRef);
-        $scope.addQuestion = function() {
 
-            questionsRef.update({q: "Testing", level: 5, answer: "YES"});
-            console.log(questionsRef);
-        };
-
-
+        //removes the first item from firebase... or maybe all of them
         $scope.answer = function() {
-            console.log($scope.questions[0]);
-            $scope.q.splice(0, 1);
-            console.log($scope.questions[0]);
             questionsRef.remove().limit(1);
         }
 
