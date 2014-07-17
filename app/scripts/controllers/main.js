64cb631780cd215b3747f55db7996c25228d1555
brendancwood <brendan@brendancwood.com>
2014-07-17 13:36:13 -0700

    Added firebase twitter auth, global chat appending of answers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,10 +7,21 @@
  * # MainCtrl
  * Controller of the realTimeTriviaApp
  */
+
 angular.module('realTimeTriviaApp')
   .controller('MainCtrl', function ($scope, $firebase) {
 
+
+    var chatRef = new Firebase('https://maxwellzirbel.firebaseio.com/');
+    var auth = new FirebaseSimpleLogin(chatRef, function(error, user) {
+      if (error) {
+        // an error occurred while attempting login
+        console.log(error);
+      } else if (user) {
+        // user authenticated with Firebase 
+        console.log(user)   
         //reference to firebase
+        var answersRef = new Firebase('https://maxwellzirbel.firebaseio.com/answers')
         var questionsRef = new Firebase("https://maxwellzirbel.firebaseio.com/questions");
         $scope.q = [];
 
@@ -20,14 +31,14 @@ angular.module('realTimeTriviaApp')
             for (var i = 0; i < $scope.q.length; i++) {
                 ref.push($scope.q[i]);
             }
-        }
+        };
 
         //this gets the first item from the list for displaying
         $scope.getFirstFromList = function(ref, cb) {
             ref.startAt().limit(1).once("child_added", function(snapshot) {
                 cb(snapshot.val());
             });
-        }
+        };
 
         //this calls make list and get first item and then alerts the value
         $scope.go = function() {
@@ -35,18 +46,51 @@ angular.module('realTimeTriviaApp')
             $scope.getFirstFromList(questionsRef, function (val) {
                 alert(val);
             });
-        }
+        };
 
         $scope.questions = $firebase(questionsRef);
 
         //this gets the first item and then deletes all of them. needs work.
         $scope.answer = function() {
+            var answer = $scope.userAnswer;
+            answersRef.set({username: user.username, answer: answer});
+            answersRef.on('value', function(snapshot){
+                var data = snapshot.val();
+                console.log(data);
+                $('.chat').append('<li>' + data.username + ': ' + data.answer + '</li>');
+            });
+            
+            $('.chat li:last-child').show('slow', function(){
+                $('.chat').animate({
+                    scrollTop: $('.chat')[0].scrollHeight}, 'slow');
+                });
+            $scope.userAnswer = '';
+
             $scope.getFirstFromList(questionsRef, function (val) {
                 $scope.q.push(val);
+                console.log($scope.q);
             });
             console.log($scope.q);
             questionsRef.remove().limit(1);
-        }
+            
+        };
+        //if user hits 'enter' to submit, call answer function, reset input text
+        $('.textAnswer').on('keypress', function(e){
+            if(e.keyCode == 13){
+                $scope.answer();
+                $(this).val('');
+            }
+        });
+        $('.chat').scrollTop($('.chat')[0].scrollHeight);
+
+
+
+          } else {
+            // user is logged out
+          }
+        });
+
+        
 
 
   });
