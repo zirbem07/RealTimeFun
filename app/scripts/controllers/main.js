35c6300f7c64a9c7b934df38dd198a5aa66e7708
zirbem07 <maxwell.zirbel@gmail.com>
2014-07-17 19:47:36 -0700

    live updating Q's working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,42 +11,54 @@ angular.module('realTimeTriviaApp')
   .controller('MainCtrl', function ($scope, $firebase) {
 
         //reference to firebase
-        var questionsRef = new Firebase("https://maxwellzirbel.firebaseio.com/questions");
-        $scope.q = [];
+        var questionsRef = new Firebase("https://maxwellzirbel.firebaseio.com/questions").limit(1);
+        $scope.questions = $firebase(questionsRef);
+        $scope.quests = '';
+        $scope.keyR = '';
+
+        $scope.init = function() {
+            $scope.data = $scope.questions;
+
+            $scope.data.$on('loaded', getFirst);
+            $scope.data.$on('change', update);
+        }
+
+        function getFirst(){
+            var keys = $scope.data.$getIndex();
+            keys.forEach(function(key, i) {
+                console.log(i, $scope.data[key]); // Prints items in order they appear in Firebase.
+                console.log($scope.keyR);
+                $scope.quests = $scope.data[key];
+                $scope.keyR = key;
+            });
+        }
+
+        function update(){
+            var keys = $scope.data.$getIndex();
+            keys.forEach(function(key, i) {
+                console.log("in update:  " + i, $scope.data[key]); // Prints items in order they appear in Firebase.
+                console.log($scope.keyR);
+                $scope.quests = $scope.data[key];
+                $scope.keyR = key;
+            });
+        }
 
         //these items will get added to the firebase
-        $scope.makeList = function(ref) {
-            $scope.q = ["This is question 1", "this is question 2", "I will make these objects", "eventually"];
+        $scope.addQuestions = function() {
+            $scope.q = [
+                {q: "This is question 1", answer: 1, level: 3},
+                {q: "this is question 2", answer: 1, level: 3},
+                {q: "I will make these objects", answer: 1, level: 4},
+                {q: "eventually", answer: 1, level: 2}];
             for (var i = 0; i < $scope.q.length; i++) {
-                ref.push($scope.q[i]);
+                $scope.questions.$add($scope.q[i]);
             }
         }
 
-        //this gets the first item from the list for displaying
-        $scope.getFirstFromList = function(ref, cb) {
-            ref.startAt().limit(1).once("child_added", function(snapshot) {
-                cb(snapshot.val());
-            });
-        }
-
-        //this calls make list and get first item and then alerts the value
-        $scope.go = function() {
-            $scope.makeList(questionsRef);
-            $scope.getFirstFromList(questionsRef, function (val) {
-                alert(val);
-            });
-        }
-
-        $scope.questions = $firebase(questionsRef);
 
         //this gets the first item and then deletes all of them. needs work.
         $scope.answer = function() {
-            $scope.getFirstFromList(questionsRef, function (val) {
-                $scope.q.push(val);
-            });
-            console.log($scope.q);
-            questionsRef.remove().limit(1);
+            //alert($scope.keyR[0]);
+            $scope.questions.$remove($scope.keyR);
         }
-
-
   });
