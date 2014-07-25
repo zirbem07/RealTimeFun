49e0e9e489c418d11ee41b6d6a364c5df53f15a7
brendancwood <brendan@brendancwood.com>
2014-07-25 00:14:31 -0700

    angularfire login with twitter

--- app/scripts/controllers/loginController.js
+++ app/scripts/controllers/loginController.js
@@ -1,17 +1,28 @@
 
 angular.module('realTimeTriviaApp')
-  .controller('loginCtrl', function ($scope, $firebase) {
-    $scope.tagline = "YOYOYO";
-    var chatRef = new Firebase("https://maxwellzirbel.firebaseio.com/");
-    var usersRef = new Firebase("https://maxwellzirbel.firebaseio.com/users");
-        var auth = new FirebaseSimpleLogin(chatRef, function(err, user){
-            if(err){
-                throw err;
-            }
-            if(user){
-                usersRef.set({uid: user.uid, username: user.username, points: 0});
-            }
-        });
+  .controller('loginCtrl', function ($scope, $firebase, $firebaseSimpleLogin) {
+    var dataRef = new Firebase("https://maxwellzirbel.firebaseio.com/");
+    $scope.loginObj = $firebaseSimpleLogin(dataRef);
+    
+        $scope.loginTwitter = function(){
+        $scope.loginObj.$login('twitter')
+            .then(function(user){
+                var username = user.username;
+                var twitterData = user.thirdPartyUserData;
+                var usersRef = new Firebase("https://maxwellzirbel.firebaseio.com/users/");
+                var usernameRef = new Firebase("https://maxwellzirbel.firebaseio.com/users/" + username);
+                $scope.usernameRef = $firebase(usersRef);
+                console.log($scope.usernameRef);
+                if($scope.usernameRef.$child(username)){
+                    console.log('exists');
+                } else{
+                    usernameRef.set({username: user.username, points: 0, profile_image_url: twitterData.profile_image_url })
+                }
+               
+            }, function(error){
+                console.log('login failed: ', error);
+            });
+        }
 
         $scope.login = function(type){
             console.log($('button').data('type'));
